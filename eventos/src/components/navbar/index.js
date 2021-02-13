7f1bb9f7f0252f960db20b06fc860770f9dfa03f
Celio Caus Junior <celiocausjunior@gmail.com>
2021-02-13 14:48:32 -0300

    Dynamic NavBar

--- eventos/src/components/navbar/index.js
+++ eventos/src/components/navbar/index.js
@@ -1,23 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import './navbar.css';
 
 
 
 const Navbar = () => {
+    const dispatch = useDispatch();
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
-                <Link to="/home" className="navbar-brand text-white font-weight-bold">Eventos</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <i className="fas fa-bars text-white"></i>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <Link className="nav-link nav-item" to="/home">Home</Link>
-                        <Link className="nav-link nav-item" to="/register">Cadastrar</Link>
-                        <Link className="nav-link nav-item" to="/login">Login</Link>
 
+                        {
+                            useSelector(state => state.loggedUser) ?
+                                <>
+                                    <Link to="/home" className="navbar-brand text-white font-weight-bold">Eventos</Link>
+
+                                    <Link className="nav-link nav-item" to="/home">Home</Link>
+                                    <Link className="nav-link nav-item" to="">Publicar Eventos</Link>
+                                    <Link className="nav-link nav-item" to="">Meus Eventos</Link>
+                                    <Link className="nav-link nav-item" onClick={() => dispatch({ type: 'LOG_OUT', })} >Sair</Link>
+                                </>
+                                :
+                                <>
+                                    <Redirect to="/login" />
+                                    <Link className="nav-link nav-item" to="/login">Login</Link>
+                                </>
+                        }
                     </div>
                 </div>
             </div>
